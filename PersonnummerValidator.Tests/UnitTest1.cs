f20b0141c7aaa2c845d3dc51bde82f0fec752a7a
sambajam <sambajam@gmail.com>
2025-02-25 21:06:42 +0100

    Add project files.

---
+++ PersonnummerValidator.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿
+using NUnit.Framework;
+
+namespace PersonnummerValidator.Tests
+{
+    public class UnitTests
+    {
+        [Test]
+        public void Test_Valid_Personnummer()
+        {
+            Assert.That(Program.IsValidSwedishPersonalNumber("800101-1234"), Is.True);
+        }
+
+        [Test]
+        public void Test_Invalid_Personnummer()
+        {
+            Assert.That(Program.IsValidSwedishPersonalNumber("123456-7890"), Is.False);
+        }
+
+        [Test]
+        public void Test_Null_Or_Empty_Personnummer()
+        {
+            Assert.That(Program.IsValidSwedishPersonalNumber(null), Is.False);
+            Assert.That(Program.IsValidSwedishPersonalNumber(""), Is.False);
+        }
+
+        [Test]
+        public void Test_Invalid_Format_Personnummer()
+        {
+            Assert.That(Program.IsValidSwedishPersonalNumber("8001011234"), Is.False);
+            Assert.That(Program.IsValidSwedishPersonalNumber("80-01-01-1234"), Is.False);
+        }
+    }
+}
---
+++ PersonnummerValidator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Write("Ange ett svenskt personnummer (ÅÅMMDD-XXXX): ");
+        string? input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input) && IsValidSwedishPersonalNumber(input))
+            Console.WriteLine("\u2705 Personnumret är giltigt!");
+        else
+            Console.WriteLine("\u274C Ogiltigt personnummer!");
+    }
+
+    public static bool IsValidSwedishPersonalNumber(string pnr)
+    {
+        if (string.IsNullOrEmpty(pnr) || !Regex.IsMatch(pnr, @"^\d{6}-\d{4}$"))
+            return false;
+
+        pnr = pnr.Replace("-", "");
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            int num = (pnr[i] - '0') * (i % 2 == 0 ? 2 : 1);
+            sum += num > 9 ? num - 9 : num;
+        }
+        int checkDigit = (10 - (sum % 10)) % 10;
+
+        return checkDigit == (pnr[9] - '0');
+    }
+}
